9cb14ab876c8a9a52b13ebbab60b323b1bfc34eb
Daniil/Pecherin <daniel.pechyorin@mail.ru>
2021-07-04 18:40:43 +0300

    Code of task№4 added

--- main3.py
+++ main3.py
@@ -83,6 +83,7 @@ class Lecturer(Mentor):
             return
         return self.count_av_lect_grade() < other.count_av_lect_grade()
 
+
 class Reviewer(Mentor):
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
@@ -94,15 +95,42 @@ class Reviewer(Mentor):
             return 'Ошибка'
 
 
+def get_av_hw_grade(all_people_list):
+    course_name = input("Введите название курса, чтобы посмотреть среднюю оценку по всем студентам: ")
+    grades_list = []
+    for person in all_people_list:
+        if isinstance(person, Student):
+            if course_name in person.courses_in_progress:
+                grades_list += person.grades[course_name]
+    return sum(grades_list) / len(grades_list)
+
+
+def get_av_lecturer_grade(all_people_list):
+    course_name = input("Введите название курса, чтобы посмотреть среднюю оценку всех лекторов: ")
+    grades_list = []
+    for person in all_people_list:
+        if isinstance(person, Lecturer) and  course_name in person.courses_attached:
+            grades_list += person.rate[course_name]
+    return sum(grades_list) / len(grades_list)
+
+#Инициализация списка, куда помещаются все преподаватели и учащиеся
+all_people_list = []
+
+#Создание экземпляров студентов и преподавателей
 student1 = Student('Иван', 'Крыльев', 'муж')
+all_people_list.append(student1)
 student2 = Student('София', 'Ковальская', 'жен')
+all_people_list.append(student2)
 student1.courses_in_progress += ['Python']
 student1.courses_in_progress += ['Git']
+student1.finished_courses += ['Введение в программирование']
 student2.courses_in_progress += ['Python']
 student2.courses_in_progress += ['Git']
 
 lecturer1 = Lecturer('Индиана', 'Джонс')
+all_people_list.append(lecturer1)
 lecturer2 = Lecturer('Иван', 'Павлов')
+all_people_list.append(lecturer2)
 lecturer1.courses_attached += ['Python']
 lecturer2.courses_attached += ['Git']
 student1.rate_lecturer(lecturer1, 'Python', 10)
@@ -110,7 +138,9 @@ student1.rate_lecturer(lecturer2, 'Git', 6)
 student2.rate_lecturer(lecturer1, 'Python', 9)
 
 reviewer1 = Reviewer('Some', 'Buddy')
+all_people_list.append(reviewer1)
 reviewer2 = Reviewer('Mister', 'Twister')
+all_people_list.append(reviewer2)
 reviewer1.courses_attached += ['Python']
 reviewer2.courses_attached += ['Git']
 reviewer1.rate_hw(student1, 'Python', 10)
@@ -121,6 +151,7 @@ reviewer2.rate_hw(student1, 'Git', 5)
 reviewer2.rate_hw(student2, 'Git', 7)
 
 
+print("ВЫПОЛНЕНИЕ ЗАДАНИЯ №3")
 print(f"Средний бал {student1.name} {student1.surname}: {student1.count_av_hw_grade()}")
 print(f"Средний бал {student2.name} {student2.surname}: {student2.count_av_hw_grade()}")
 print(f"student1 < student2: {student1 < student2}")
@@ -128,36 +159,13 @@ print()
 print(f"Средний бал лектора {lecturer1.name} {lecturer1.surname}: {lecturer1.count_av_lect_grade()}")
 print(f"Средний бал лектора {lecturer2.name} {lecturer2.surname}: {lecturer2.count_av_lect_grade()}")
 print(f"lecturer1 > lecturer2: {lecturer1 > lecturer2}")
+print()
 
+print("ВЫЗОВ МЕТОДОВ.ЗАДАНИЕ №4")
+print(f"Средний бал за ДЗ для {student1.name} {student1.surname}: {student1.count_av_hw_grade()}")
+print(f"Средний бал лектора {lecturer1.name} {lecturer1.surname}: {lecturer1.count_av_lect_grade()}")
+print()
 
-
-
-
-
-
-
-
-
-
-
-# print(best_student.courses_in_progress)
-#
-# cool_reviewer = Reviewer('Some', 'Buddy')
-# cool_reviewer.courses_attached += ['Python']
-# cool_reviewer.rate_hw(best_student, 'Python', 10)
-# cool_reviewer.rate_hw(best_student, 'Python', 10)
-# cool_reviewer.rate_hw(best_student, 'Python', 10)
-#
-# cool_lecturer = Lecturer('Indiana', 'Jones')
-# cool_lecturer.courses_attached += ['Python']
-# cool_lecturer.courses_attached += ['Git']
-# print(cool_reviewer.courses_attached)
-# best_student.rate_lecturer(cool_lecturer, 'Python', 10)
-# best_student.rate_lecturer(cool_lecturer, 'Git', 8)
-# best_student.rate_lecturer(cool_lecturer, 'Python', 9)
-#
-# print(best_student.name, best_student.surname, "hw grades: ", best_student.grades)
-# print(cool_lecturer.name, cool_lecturer.surname, "'s lectures grades: ", cool_lecturer.rate)
-# print(cool_lecturer)
-# print()
-# print(best_student)
+print("ВЫПОЛНЕНИЕ МЕТОДОВ, СОЗДАННЫХ ДЛЯ ЗАДАНИЯ №4")
+print(get_av_hw_grade(all_people_list))
+print(get_av_lecturer_grade(all_people_list))
